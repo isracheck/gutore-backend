73a54712d58dbccdf428ced821b19ab6a170bd16
Israel Gutierrez <israelgutierrez@Israels-MacBook-Pro.local>
2021-10-26 00:04:38 +0200

    first commit

---
+++ src/main/java/com/gutore/websitebackend/GutoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gutore.websitebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GutoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GutoreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gutore/websitebackend/GutoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gutore.websitebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GutoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
