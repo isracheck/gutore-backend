17a1cd4404fce14fe97a4d8f00f697e24a2ac7a0
Israel Gutierrez <israelgutierrez@Israels-MacBook-Pro.local>
2021-11-04 01:00:56 +0100

    format

--- src/main/java/com/gutore/websitebackend/entity/Company.java
+++ src/main/java/com/gutore/websitebackend/entity/Company.java
@@ -14,153 +14,100 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-
-@Entity 
-@Table(name="gocompany")
+@Entity
+@Table(name = "gocompany")
 public class Company implements Serializable {
 
 	@Id
-	@Column(name="idcompany")
+	@Column(name = "idcompany")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idCompany;
-	
+
 	@Column(nullable = false)
 	private String name;
-	
-	@Column(name="docnumber", unique = true, nullable = false)
+
+	@Column(name = "docnumber", unique = true, nullable = false)
 	private String docNumber;
-	
-	@Column(name="doctype", nullable = false)
+
+	@Column(name = "doctype", nullable = false)
 	private String docType;
-	
+
 	private String logo;
-	
+
 	@Column(nullable = false)
 	private String email;
-	
+
 	@Column(nullable = false)
 	private String pass;
-	
-	
+
 	@OneToMany(cascade = CascadeType.ALL)
 	@JoinColumn(name = "idcompany", referencedColumnName = "idcompany")
 	@Column(nullable = false)
 	private List<Slot> slot = new ArrayList<>();
-	
-	
-
 
 	public Long getIdCompany() {
 		return idCompany;
 	}
 
-
-
-
 	public void setIdCompany(Long idCompany) {
 		this.idCompany = idCompany;
 	}
-
-
-
 
 	public String getName() {
 		return name;
 	}
 
-
-
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
-
-
 
 	public String getDocNumber() {
 		return docNumber;
 	}
 
-
-
-
 	public void setDocNumber(String docNumber) {
 		this.docNumber = docNumber;
 	}
-
-
-
 
 	public String getDocType() {
 		return docType;
 	}
 
-
-
-
 	public void setDocType(String docType) {
 		this.docType = docType;
 	}
-
-
-
 
 	public String getLogo() {
 		return logo;
 	}
 
-
-
-
 	public void setLogo(String logo) {
 		this.logo = logo;
 	}
-
-
-
 
 	public String getEmail() {
 		return email;
 	}
 
-
-
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
-
-
 
 	public String getPass() {
 		return pass;
 	}
 
-
-
-
 	public void setPass(String pass) {
 		this.pass = pass;
 	}
-
-
-
 
 	public List<Slot> getSlot() {
 		return slot;
 	}
 
-
-
-
 	public void setSlot(List<Slot> slot) {
 		this.slot = slot;
 	}
-
-
-
 
 	private static final long serialVersionUID = 1L;
 }
--- src/main/java/com/gutore/websitebackend/entity/Slot.java
+++ src/main/java/com/gutore/websitebackend/entity/Slot.java
@@ -13,97 +13,64 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="goslots")
+@Table(name = "goslots")
 public class Slot implements Serializable {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@Column(nullable = false)
 	private Date day;
-	
-	@Column (name="ihour", nullable = false)
+
+	@Column(name = "ihour", nullable = false)
 	private Date initialHour;
-	
-	@Column (name="fhour", nullable = false)
+
+	@Column(name = "fhour", nullable = false)
 	private Date finalHour;
-	
-	@Column (name="slots", nullable = false)
+
+	@Column(name = "slots", nullable = false)
 	private Integer numberSlots;
-	
-	
-	
-	
+
 	public Long getId() {
 		return id;
 	}
-
-
-
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
-
-
 	public Date getDay() {
 		return day;
 	}
-
-
-
 
 	public void setDay(Date day) {
 		this.day = day;
 	}
 
-
-
-
 	public Date getInitialHour() {
 		return initialHour;
 	}
-
-
-
 
 	public void setInitialHour(Date initialHour) {
 		this.initialHour = initialHour;
 	}
 
-
-
-
 	public Date getFinalHour() {
 		return finalHour;
 	}
-
-
-
 
 	public void setFinalHour(Date finalHour) {
 		this.finalHour = finalHour;
 	}
 
-
-
-
 	public Integer getNumberSlots() {
 		return numberSlots;
 	}
 
-
-
-
 	public void setNumberSlots(Integer numberSlots) {
 		this.numberSlots = numberSlots;
 	}
-
-
-
 
 	private static final long serialVersionUID = 1L;
 }
